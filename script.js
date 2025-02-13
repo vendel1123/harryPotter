da12a2cd674167495faf0aa9c398392e9da7511c
vendel1123 <vendelhegyi68@gmail.com>
2025-02-13 11:01:21 +0100

    fetch - response

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 window.addEventListener('DOMContentLoaded', fetchCharacters);
 
 async function fetchCharacters(){
-    
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
+        console.log(response);
+    } catch (error) {
+        
+    }
 }
