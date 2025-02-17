74749383487155eaf33e0c8eeaa4619e2992a961
vendel1123 <vendelhegyi68@gmail.com>
2025-02-17 13:19:28 +0100

    változás a script.js-ben

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ window.addEventListener('DOMContentLoaded', fetchCharacters);
 async function fetchCharacters(){
     try {
         const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
-        console.log(response);
+        //console.log(response);
+
+        const characters = await response.json();
+        console.log(characters);
+
     } catch (error) {
-        
+        console.error('Hiba történt az adatok lekérésekor:', error);
     }
 }
